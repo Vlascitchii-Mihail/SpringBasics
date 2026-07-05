5a27a0b38408e3f48c2164cf5fca9517d2142aa9
Mihail <mishavlashitschii@gmail.com>
2026-07-05 12:07:02 +0300

    TSK-ID-7 WebClient implementation

--- src/main/java/com/spring_fast/main/config/Config.java
+++ src/main/java/com/spring_fast/main/config/Config.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.spring_fast.main.proxy")
@@ -12,5 +13,10 @@ public class Config {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 }
